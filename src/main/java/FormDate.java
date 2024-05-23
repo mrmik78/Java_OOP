2a8baa02689cdad435214f2dfbf7df6a89b2a3ec
mrmik78 <m.reutsky@mail.ru>
2024-05-23 21:09:07 +0300

    update

--- src/main/java/FormDate.java
+++ src/main/java/FormDate.java
@@ -2,6 +2,4 @@ public class FormDate {
     public int day;
     public int month;
     public int year;
-
-
 }
--- src/main/java/Post.java
+++ src/main/java/Post.java
@@ -7,4 +7,3 @@ public class Post {
     public boolean subscription;
     public FormDate birthday;
 }
-
